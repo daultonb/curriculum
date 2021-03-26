7c1ed4a0ec65a95eaa2137c09ef0d9ac10373173
Daulton Baird <daultonbaird1@gmail.com>
2021-03-25 17:21:38 -0700

    landing page and partial about
    about is WIP, landing page is done. Need to ask about implementing UBC CLF css for accordion.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-
+<link rel="stylesheet" href="https://cdn.ubc.ca/clf/7.0.4/css/ubc-clf-full-bw.min.css">
 
 <!-- Code can goes down here -->
 
@@ -10,7 +10,30 @@
     <div class="row">
 
         <div style="width: 100%;border-bottom: 1px solid #DCDCDC">
-        <h1 style="text-align:center;">About</h1>
+            <h1 style="text-align:center;">About</h1>
+
+        </div>
+        <div class="ok-accordion-wrapper">
+                <div id="ok-accordion-1" class="ok-accordion ok-accordion-blue">
+                    <div class="accordion-group">
+                        <div class="accordion-heading">
+                            <a class="accordion-toggle" href="#section-2" data-toggle="collapse" data-parent="ok-accordion-1">What is Curriculum Mapping</a>
+                        </div>
+                        <div id="section-2" class="accordion-body in collapse" style="height:auto;">
+                            <div class="accordion-inner">
+                                <p>Curriculum mapping is an instrument to evaluate the alignment of learning objectives to academic strategies at educational institutions. The mapping process is an effective way to - </p>
+                                <ul>
+                                    <li>“make learning and teaching more meaningful to students and teachers” (Lam and Tsui, 2016); </li>
+                                    <li>“develop, review, improve and perfect an integrated curriculum, including curriculum alignment” (Khoerunnisa et al, 2018); and </li>
+                                    <li>“understand curriculum structures and relationships, gain insight in how students experience their discipline, and increase awareness of curricular content” (Archambault and Masunaga, 2015). </li>
+                                </ul>
+                                <p><a href="https://strategicplan.ubc.ca/transformative-learning/">UBC’s Strategic Plan’s Strategy on Transformative Learning</a> focuses on an important cornerstone of the curriculum mapping process. The strategy notes that: </p>
+                                <p><i>Our work at UBC is focused on enhanced support for program redesign around competencies; the development of problem-solving experiences; technology-enabled learning; and continued growth in work-integrated education. Sustained progress requires leadership across the institution to model, inspire and celebrate excellence in teaching and mentorship. </i></p>
+                                <p>To create an immersive learning experience that fosters competencies identified by faculties and departments as critical to the discipline, a curriculum map can be an <strong>effective way to visualize the program structure, list of courses, teaching and learning activities, and assessment practices.</strong> This “integrative and sustainable” (Kalu & Dyjur, 2018) process is collaborative and <strong>geared towards student success.</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
         </div>
 
     </div>
--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -9,10 +9,10 @@
                 <p class="lead">Course and program planning platform at UBCO</p>
                 <hr class="my-4">
                 <p>Plan, review and align courses and programs using online curriculum mapping tool. </p>
-                 <p class="lead">
+                <p class="lead">
                     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                 </p>
-             </div>
+            </div>
         </div>
     </div> -->
 
@@ -35,12 +35,27 @@
             <div class="jumbotron">
                 <h1 class="display-4">Curriculum Alignment Tool (CAT)</h1>
                 <p class="lead">This web application aims to support instructors to ideate, evaluate or create new programs and courses from a <a href="https://www.heacademy.ac.uk/sites/default/files/resources/id477_aligning_teaching_for_constructing_learning.pdf" target="_blank"><strong>constructive alignment</strong></a> perspective.</p>
+                <ul>
+                    <li class="lead">Overview</li>
+                    <ul>
+                        <li class="lead">Support instructors with the <strong> ideation of a new program or redesign of an existing one,</strong> by allowing them to map program learning outcomes to the learning outcomes of required and non-required courses for the program</li>
+                        <li class="lead">Support instructors with the <strong>evaluation of courses</strong> from a constructive alignment perspective and backward design approach. That is, by looking at the alignment across course learning outcomes, assessment methods, and teaching and learning activities.</li>
+                        <li class="lead">Support the ideation phase of <strong>creating a new course</strong> by identifying course learning outcomes, assessment strategies, and teaching and learning methods.</li>
+                    </ul>
+                    <li class="lead">How to use this website</li>
+                </ul>
+                <p class="lead">In order to use this website, users must have identified the course or program learning outcomes. The below information should also be available for better results:</p>
+                <ol>
+                    <li class="lead">Assessment methods (e.g. quizzes, oral presentation, research paper, etc.)</li>
+                    <li class="lead">Teaching and learning activities (e.g. lecture, problem-based learning, lab, tutorial, discussion, etc.)</li>
+                </ol>
+                <p class="lead">Be ready to input this information when prompted by the website. The website will walk you through a series of steps ending with a summary of your curriculum alignment at the course or program level. The information you input can be modified at any time.</p>
                 <!-- <hr class="my-4">
                 <p>Plan, review and align courses and programs using online curriculum mapping tool. </p>
-                 <p class="lead">
+                <p class="lead">
                     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                 </p> -->
-             </div>
+            </div>
         </div>
     </div>
 
@@ -54,32 +69,32 @@
             </p>
             <div class="card-deck">
                 <div class="card">
-                  <div class="card-body">
+                <div class="card-body">
 
 
                     <p class="card-text"><strong >IDEATION </strong>of a new program. It allows instructors to map program learning outcomes (PLOs) to course learning outcomes (CLOs) of required and non-required courses for the program. </p>
 
-                  </div>
+                </div>
                 </div>
                 <div class="card">
 
-                  <div class="card-body">
+                <div class="card-body">
 
 
                     <p class="card-text"><strong>EVALUATION</strong> of existing courses or programs by looking at the alignment across learning outcomes, assessment methods, and teaching and learning activities.</p>
 
-                  </div>
+                </div>
                 </div>
                 <div class="card">
 
-                  <div class="card-body">
+                <div class="card-body">
 
 
                     <p class="card-text"><strong>CREATION</strong> of a new course by identifying course learning outcomes, assessment strategies, and teaching and learning methods.</p>
 
-                  </div>
                 </div>
-              </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -87,7 +102,7 @@
         <div class="col-md-12 bg-primary">
             <div class="my-5">
 
-               <div class="container">
+            <div class="container">
                 <div class="row">
                     <!-- <div class="col-md">
                         <h1>1. <p class="lead">Create a new program project</p></h1>
