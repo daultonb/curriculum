4bf5cb1c580583f2c0e755d402b622bd21b9e62b
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2021-05-20 12:12:55 -0700

    Update Verify.Blade
    Verify.blade now shows an alert to inform the user to be sure to check their spam/junk folder in their mailbox

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,7 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if (!session('resent'))
+                <div class="alert alert-success" role="alert">
+                    {{ __('An email has been sent to you for verification. Please be sure to check your spam/junk folder.') }}
+                </div>
+            @endif
             <div class="card">
+                <div class="card-body">
                 <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
