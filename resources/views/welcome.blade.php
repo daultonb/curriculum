8bc76d601c632be7180f1273cd89622151dcb52e
Daulton Baird <daultonbaird1@gmail.com>
2021-06-03 10:23:56 -0700

    Update welcome.blade.php
    Testing forking for PR

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,6 +125,7 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Build v{{ Illuminate\Foundation\Application::VERSION }}
                     </div>
+                    <p> This is added to test forking in Git</p>
                 </div>
             </div>
         </div>
