27698b65de5bf3e517e19d364b807d0dd1767c6b
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2021-06-09 14:56:05 -0700

    minor update to image to external links

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -64,8 +64,7 @@
                 <h1> Our Goals </h1>
                 <p class="lead">UBC Okanagan is committed to providing the highest quality of education to all students. This means ongoing work for continuous improvement and innovation of teaching and learning practices across disciplines.</p>
                 <p class="lead">This website aims to support this commitment by providing all instructors with a tool to ideate, create, and evaluate new or already existing courses and programs, using backward design. Engaging in this important exercise benefits students, instructors, and our overall communities. </p>
-                <i class="bi bi-box-arrow-up-right"></i>
-                <a href="https://provost.ok.ubc.ca/initiatives/quality-assurance-and-enhancement/" title="Quality Assurance and Enhancement site"  target="_blank" style="color: white;">Learn more about UBC Okanagan’s efforts towards quality assurance and enhancement</a>
+                <a href="https://provost.ok.ubc.ca/initiatives/quality-assurance-and-enhancement/" title="Quality Assurance and Enhancement site"  target="_blank" style="color: white;"><i class="bi bi-box-arrow-up-right"></i> Learn more about UBC Okanagan’s efforts towards quality assurance and enhancement</a>
             </div>
         </div>
     </div>
@@ -152,7 +151,7 @@
                 <br>
                 <div class="row">
                 <p class="lead">Funded by the UBC Okanagan Office of the Provost and Vice President Academic.</p>
-                <p class="lead">Inspired by and based on UCalgary’s tool <a href="https://taylorinstitute.ucalgary.ca/curriculum-links" target="_blank">Curriculum Links</a>.</p>
+                <p class="lead">Inspired by and based on UCalgary’s tool <a href="https://taylorinstitute.ucalgary.ca/curriculum-links" target="_blank"><i class="bi bi-box-arrow-up-right"></i> Curriculum Links</a>.</p>
                 </div>
             </div>
         </div>
@@ -162,7 +161,7 @@
                 <div class="col-md">
                     <h1><strong>Questions ?</strong></h1>
                     <p class="lead">
-                        If you have questions about the Curriculum MAP, please contact <a href="mailto:laura.prada@ubc.ca" target="_blank">laura.prada@ubc.ca</a> at the <a href="https://provost.ok.ubc.ca/" target="_blank">Office of the Provost and Vice-President Academic, UBC Okanagan campus</a>.
+                        If you have questions about the Curriculum MAP, please contact <a href="mailto:laura.prada@ubc.ca" target="_blank">laura.prada@ubc.ca</a> at the <a href="https://provost.ok.ubc.ca/" target="_blank"><i class="bi bi-box-arrow-up-right"></i> Office of the Provost and Vice-President Academic, UBC Okanagan campus</a>.
                     </p>
 
                 </div>
