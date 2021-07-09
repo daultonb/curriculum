647c8b39158f62ab2982a8a909cee9c1f084224d
rmeofone <46071994+rmeofone@users.noreply.github.com>
2021-07-08 18:23:00 -0700

    forgot to add sidebar view changes to backpack

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -3,3 +3,5 @@
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('course') }}'><i class='nav-icon la la-question'></i> Courses</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('program') }}'><i class='nav-icon la la-question'></i> Programs</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('standard-category') }}'><i class='nav-icon la la-question'></i> Standard Categories</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('standards-scale-category') }}'><i class='nav-icon la la-question'></i> Standards Scale Categories</a></li>
