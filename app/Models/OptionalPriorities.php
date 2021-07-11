0f1e7f356bbb0e7327818c6e1410ae11bfeb50a9
MichaelOdermatt <michaelodermatt@outlook.com>
2021-07-11 15:05:34 -0700

    added op_id to fillable

--- app/Models/OptionalPriorities.php
+++ app/Models/OptionalPriorities.php
@@ -15,7 +15,7 @@ class OptionalPriorities extends Model
     protected $table = 'optional_priorities';
     protected $primaryKey = 'op_id';
     protected $guarded = ['op_id'];
-    protected $fillable = ['subcat_id','optional_priority'];
+    protected $fillable = ['op_id', 'subcat_id','optional_priority'];
     
     public function OptionalPrioritySubcategories(){
         return $this -> belongsTo(OptionalPrioritySubcategories::class, 'subcat_id', 'subcat_id');
