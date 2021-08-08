5f3f9c7f52b35750169756aaf36c6d675382dc3c
MichaelOdermatt <michaelodermatt@outlook.com>
2021-08-08 14:32:24 -0700

    Added css styling to plos when the delete button is clicked

--- app/Http/Controllers/Admin/CourseCrudController.php
+++ app/Http/Controllers/Admin/CourseCrudController.php
@@ -540,7 +540,9 @@ class CourseCrudController extends CrudController
             $delFoo1 = "onClick=\"(function(){\n"
                             . "$(&quot;tr[id=cp_";
             $delFoo2 = "_";
-            $delFoo3 = "] td input&quot;).prop(&quot;checked&quot;, false)\n"
+            $delFoo3 = "] td input&quot;).prop(&quot;checked&quot;, false);\n";
+            $delFoo4 = "$(&quot;tr[id=cp_";
+            $delFoo5 = "]&quot;).css(&quot;text-decoration&quot;, &quot;line-through&quot;);n"
                           . "})();\"";
             
             foreach ($Progs as $program){ 
@@ -570,7 +572,7 @@ class CourseCrudController extends CrudController
                             if($i <= $MScales->count())$val = "value=\"".$MScales[$i-1]->map_scale_id."\"";
                             $custHTML.="<td width=\"6%\"><input type=\"radio\" name=\"map_".$clo->l_outcome_id."_".$plo->pl_outcome_id."[]\" $chk $val></td>";
                         }
-                        $delBstr = $delFoo1.$clo->l_outcome_id.$delFoo2.$plo->pl_outcome_id.$delFoo3;
+                        $delBstr = $delFoo1.$clo->l_outcome_id.$delFoo2.$plo->pl_outcome_id.$delFoo3.$delFoo4.$clo->l_outcome_id.$delFoo2.$plo->pl_outcome_id.$delFoo5;
                         $custHTML.="<td width=\"4%\">"
                                 . "<button class=\"btn btn-sm btn-light removeItem\" type=\"button\" $delBstr>"
                                 . "<span class=\"sr-only\">delete item</span>"
